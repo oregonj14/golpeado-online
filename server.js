0840d8c59a21716a896a1c1b3125f0197c0bfc68
oregonj14 <ankenurr@gmail.com>
2026-05-31 06:07:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -423,4 +423,5 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => console.log('Servidor de Golpeado corriendo en puerto 3000'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
